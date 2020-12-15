22cfa753e0d1993db0769b0775eef2fd02fa6a0d
David Kilonzo <binginaya708@gmail.com>
2020-12-15 22:12:00 +0300

    modify models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 from . import db
-from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
