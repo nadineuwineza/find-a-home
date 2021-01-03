038bb33208223f0f647133c088111677da6f659f
nadineuwineza <nadineuwineza2017@gmail.com>
2021-01-03 21:19:21 +0200

    initial commit

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:dellakomar24@localhost/home'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nadine:Uwineza123@@localhost/home'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
@@ -17,10 +17,10 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'dellakomar27@gmail.com'
-    MAIL_PASSWORD = '0726113175'
+    MAIL_USERNAME = 'nadineuwineza2007@gmail.com'
+    MAIL_PASSWORD = 'Uwineza123@'
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SENDER_EMAIL = 'dellakomar27@gmail.com'
+    SENDER_EMAIL = 'nadineuwineza2017@gmail.com'
 
 
     # simple mde  configurations
@@ -32,7 +32,7 @@ class ProdConfig(Config):
 
 pass
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:dellakomar24@localhost/home'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nadine:Uwineza123@@localhost/home'
 
 
 class DevConfig(Config):
@@ -43,7 +43,7 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:dellakomar24@localhost/home'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nadine:Uwineza123@@localhost/home'
 
     DEBUG = True
 
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY='teamkubwa'
-export MAIL_USERNAME='dellakomar27@gmail.com'
-export MAIL_PASSWORD='0726113175'
+export MAIL_USERNAME='nadineuwineza2017@gmail.com'
+export MAIL_PASSWORD='Uwineza123@'
 
 
-python3.6 manage.py server
+python3.8 manage.py server
